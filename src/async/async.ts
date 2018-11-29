bf5502a293b5fca15520cd9da6d48b67fde4d44b
Nicklas Pouey-Winger <winger.nicklas@gmail.com>
2018-11-29 10:41:15 +0100

    Add manual subscription to store in constructor

--- src/async/async.ts
+++ src/async/async.ts
@@ -1,32 +1,33 @@
 import { autoinject } from "aurelia-framework";
-import { connectTo } from "aurelia-store";
+import { Store } from "aurelia-store";
 import { Subscription } from "rxjs";
 
 import { State } from "state";
 
 @autoinject
-@connectTo({
-  selector: store => store.state,
-  setup: "attached"
-})
 export class async {
   public data: any[];
   public state: State;
 
   private subscription: Subscription;
 
-  constructor() {
+  constructor(private store: Store<State>) {
+    this.subscription = this.store.state.subscribe(
+      (state: State) => (this.state = state)
+    );
+
     if (this.state) {
+      console.log(this.state);
       // What if I want to access a slice of the state here?
       // The point would be to avoid duplicate service calls across the app and load them from app.ts
     }
   }
 
   attached() {
-    console.log(this.state); // Yields {emails: Array(0)}
+    console.log(this.state);
   }
 
   activate() {
-    console.log(this.state); // Yields undefined, probably due to setup: "attached"
+    console.log(this.state);
   }
 }
--- src/home/home.ts
+++ src/home/home.ts
@@ -1,3 +1,23 @@
+import { autoinject } from "aurelia-framework";
+import { Subscription } from "rxjs";
+import { State } from "state";
+import { Store } from "aurelia-store";
+@autoinject
 export class Home {
+  public state: State;
+
+  private subscription: Subscription;
+
   message = "Home";
+
+  constructor(private store: Store<State>) {
+    this.subscription = this.store.state.subscribe(
+      (state: State) => (this.state = state)
+    );
+    this.functionDependingOnState();
+  }
+
+  private functionDependingOnState() {
+    console.log(this.state.emails); // Yields Array(0) - Still not being populated in time
+  }
 }
